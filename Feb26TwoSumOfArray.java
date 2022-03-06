bb39f9d20183165b5a0ba45391df8781caa69aa2
Mr. Developer <31615147+Renukacharya@users.noreply.github.com>
2022-03-06 19:28:15 +0530

    Add files via upload

---
+++ Feb26TwoSumOfArray.java
@@ -0,0 +1,23 @@
+package dsaFeb2022;
+import java.util.*;
+
+public class Feb26TwoSumOfArray {
+	static int[] targetSum(int []a , int target)
+    {
+        for(int i = 0 ; i < a.length - 1 ; i++)
+            for(int j = i + 1 ; j < a.length ; j++)
+            {
+                if(a[i] + a[j] == target)
+                    return new int[]{i + 1 , j + 1};
+            }
+        return new int[]{-1 , -1};
+    }
+    public static void main(String args[])
+    {
+        int [] a = {1 , 4 , 5 , 11 , 12};
+        int target = 5;
+        for(int x : targetSum(a , target))
+            System.out.print(x + " ");
+    }
+	        
+}
